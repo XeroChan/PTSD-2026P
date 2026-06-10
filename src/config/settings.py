902a3c7be549f4b469f523e6f9e146696ab73e06
Krzysztof Tadeusiak <01169638@pw.edu.pl>
2026-06-10 18:11:18 +0200

    feat: kaffka configuration via environmantal changes

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,4 +1,9 @@
-KAFKA_BROKER = "kafka:29092"
-TOPIC_RAW = 'raw_transactions'
-TOPIC_ALARMS = 'alarms'
-FLINK_GROUP_ID = 'flink-anomaly-group'
+import os
+
+# Adres brokera Kafki. Domyślnie 'kafka:29092' (komunikacja wewnątrz sieci Dockera).
+# Uruchamiając z hosta, ustaw zmienną środowiskową, np. KAFKA_BROKER=localhost:9092
+KAFKA_BROKER = os.environ.get("KAFKA_BROKER", "kafka:29092")
+
+TOPIC_RAW = os.environ.get("TOPIC_RAW", "raw_transactions")
+TOPIC_ALARMS = os.environ.get("TOPIC_ALARMS", "alarms")
+FLINK_GROUP_ID = os.environ.get("FLINK_GROUP_ID", "flink-anomaly-group")
